ea000addb6d2031b95ae6454f54de9a3a8bc2fec
matheusvrau888 <143196437+matheusvrau888@users.noreply.github.com>
2023-11-03 11:27:58 -0300

    So um treino
    Meu primeiro Commit 03/11/2023

---
+++ Untitled.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `follows` (
+  `cpf` 12345678910 PRIMARY KEY,
+  `nome` Matheus,
+  `sobrenome` silva,
+  `idade` 19
+);
+
+CREATE TABLE `cadastro_pj` (
+  `cnpj` interger PRIMARY KEY,
+  `nome` Matheus,
+  `escricao` 362515,
+  `ipoatividade` seila
+);
+
+CREATE TABLE `produto` (
+  `cnpj` 123456789789 PRIMARY KEY,
+  `nome` Mercado,
+  `country_code` compras
+);
